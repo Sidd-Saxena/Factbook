35b5891818babfb86bfa1b39436f6bf1f7ebf37a
Siddharth Saxena <sid@getlatch.com>
2016-10-18 15:07:12 -0400

    added a sessions model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup'  => 'users#new'
   resources :users
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
 end
